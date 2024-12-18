46e7db0b63889eb89822d5e39e1d3b90e88f281b
Gaurav <myofficialgaurav@gmail.com>
2024-12-18 15:32:03 +0000

    Comparision

---
+++ 01_BASIC/4_Comparision.js
@@ -0,0 +1,22 @@
+/*
+console.log(2 >= 1);
+console.log(2 > 1);
+console.log(2 <= 1);
+console.log(2 != 1);
+console.log(2 == 1);
+console.log(2 === 1); //yaha datatype bhi check hoga
+*/
+
+//console.log("2" > 1)
+//console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0); //because > and < implies to convert null into 0 but == does not implie this
+console.log("Now")
+console.log("2" == 2)
+console.log("2" === 2);
