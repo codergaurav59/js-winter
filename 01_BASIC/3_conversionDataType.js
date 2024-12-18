d24a12cf7f44f861effb90adb1594c00e7305fdb
Gaurav <myofficialgaurav@gmail.com>
2024-12-18 15:02:16 +0000

    Conversion of dataTypes

---
+++ 01_BASIC/3_conversionDataType.js
@@ -0,0 +1,27 @@
+let score = "33"
+console.log(typeof(score));
+console.log(typeof score);
+
+let scoreinNumber = Number(score)
+//if score = 
+// "acs" -> NaN, true
+//  null -> 0, false
+//  undefined-> NaN, false
+// "33" -> 33, true
+// true -> 1, false -> 0
+
+console.log(scoreinNumber)
+
+let scoreinBool = Boolean(score)
+console.log(scoreinBool);
+
+let num = undefined
+let numinStr = String(num)
+console.log(numinStr);
+
+console.log(typeof(numinStr));
+
+
+
+
+
